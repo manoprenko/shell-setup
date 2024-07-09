79681ef0f5c6b39e96e67b3175311e2dbb4a0e62
Michael Anoprenko <manoprenko@gmail.com>
2024-07-09 14:40:02 +0200

    wip

--- setup.sh
+++ setup.sh
@@ -7,3 +7,6 @@ sudo chsh -s $(which zsh) $whoami
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
+
+curl -fsSL https://raw.githubusercontent.com/manoprenko/shell-setup/main/zshrc > ~/.zshrc
+source ~/.zshrc
