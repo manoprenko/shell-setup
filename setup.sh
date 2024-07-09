a46caa319e186aef5d2fd944297ad0b6e4b0db76
Michael Anoprenko <manoprenko@gmail.com>
2024-07-09 14:52:37 +0200

    wip

--- setup.sh
+++ setup.sh
@@ -9,4 +9,9 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
 
 curl -fsSL https://raw.githubusercontent.com/manoprenko/shell-setup/main/zshrc > ~/.zshrc
+
+mkdir -p ~/.ssh
+echo 'ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBABrcszPFREqHsQ8UBc1084ZbbyDaAdzTclJR8sETtitbTI/nH/RQBA4sGmV1s9fGo99Nz1fBdRTdZqBZ4lM/kOOgwGF6yZBZ9OVhX7Xbnnm1FNA1cadtkBnQEQyE4EOgtHIbeQ9RmBfCRn/RK6JyE67BvY0nr/B6haoGRW8AFcR6KO+1w== michael@michael-macbook.local' >> ~/.ssh/authorized_keys
+
+exec $(which zsh)
 source ~/.zshrc
