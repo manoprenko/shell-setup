95580c0a38ed2c83da9014c0a3a2013d6aedff56
Michael Anoprenko <manoprenko@gmail.com>
2024-07-09 14:28:04 +0200

    Init

---
+++ setup_shell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get -y install zsh curl git
+sudo chsh -s $(which zsh) $whoami
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
