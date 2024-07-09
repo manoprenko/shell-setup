e8f8d329ea4976391b166f13e39fadabf76f4ee3
Michael Anoprenko <manoprenko@gmail.com>
2024-07-09 14:49:47 +0200

    wip

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 sudo apt-get -y install zsh curl git thefuck
-sudo chsh -s $(which zsh) $whoami
+sudo chsh -s $(which zsh) $(whoami)
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
