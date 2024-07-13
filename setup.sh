12aca1e05fd1eb03303f2d6624c2f03be120e20e
Michael Anoprenko <manoprenko@gmail.com>
2024-07-13 14:55:25 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,3 +12,9 @@ curl -fsSL https://raw.githubusercontent.com/manoprenko/shell-setup/main/zshrc >
 
 mkdir -p ~/.ssh
 echo 'ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBABrcszPFREqHsQ8UBc1084ZbbyDaAdzTclJR8sETtitbTI/nH/RQBA4sGmV1s9fGo99Nz1fBdRTdZqBZ4lM/kOOgwGF6yZBZ9OVhX7Xbnnm1FNA1cadtkBnQEQyE4EOgtHIbeQ9RmBfCRn/RK6JyE67BvY0nr/B6haoGRW8AFcR6KO+1w== michael@michael-macbook.local' >> ~/.ssh/authorized_keys
+
+mkdir -p ~/.bin
+cd ~/.bin
+curl https://getmic.ro | bash
+
+cd
