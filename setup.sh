d70af011dc4f71f247ae64269ee8ab49d2226449
Michael Anoprenko <manoprenko@gmail.com>
2025-09-08 01:17:45 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get -y install zsh curl git thefuck
+sudo apt-get -y install zsh curl git
 sudo chsh -s $(which zsh) $(whoami)
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
